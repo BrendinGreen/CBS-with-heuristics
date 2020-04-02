558b6a2f80349f7615faf0f0a1b40d34980bad26
blvnder <alicelinlc@gmail.com>
2020-04-02 00:33:33 -0700

    Start MDD

--- improved_cbs.py
+++ improved_cbs.py
@@ -1,8 +1,7 @@
 import time as timer
 import heapq
 import random
-import copy
-from single_agent_planner import compute_heuristics, a_star, get_location, get_sum_of_cost
+from single_agent_planner import *
 
 
 def detect_collision(path1, path2):
@@ -51,17 +50,82 @@ def detect_collisions(paths):
     return collisions
 
 
-def detect_cardinal_conflict(collisions):
-    # Iterate over all collisions
+def build_mdd(my_map, start_loc, goal_loc, agent, constraints, depth):
+    """ my_map      - binary obstacle map
+        start_loc   - start position
+        goal_loc    - goal position
+        agent       - the agent that is being re-planned
+        depth       - cost of parent node N.cost
+    """
 
-        # Test each collision C = (a_i, a_j, D, t) for cardinality by building 
-        # MDD_i and MDD_j and checking whether their width at depth t is 1
+    def push(open_list, node):
+        heapq.heappush(open_list, (node['g_val'], node['loc'], node))
 
-    # If no cardinal conflict is found, a semi-cardinal conflict is chosen
+    def pop(open_list):
+        _, _, curr = heapq.heappop(open_list)
+        return curr
+
+    def compare(n1, n2):
+        """Return true is n1 is better than n2."""
+        return n1['g_val'] < n2['g_val']
+
+    # Perform a breadth-first search from the start location of the agent down to depth and 
+    # only store the partial DAG which starts at start_loc and ends at goal_loc at depth
+
+    open_list = []
+    closed_list = dict()
+    constraint_table = build_constraint_table(constraints, agent)
+
+    root = {'loc': start_loc, 'g_val': 0, 'parent': None, 'timestep': 0}
+    push(open_list, root)
+    closed_list[(root['loc'], root['timestep'])] = root
+
+    while len(open_list) > 0:
+        curr = pop(open_list)
+        
+        if curr['loc'] == goal_loc:
+            res = [v for k, v in constraint_table.items() if k > curr['timestep']]
+            if res:
+                pass
+            else:
+                return get_path(curr)
+
+        for dir in range(5):
+            child_loc = move(curr['loc'], dir)
+            if (child_loc[0] >= len(my_map) or child_loc[0] == -1) or (child_loc[1] >= len(my_map[0]) or child_loc[1] == -1):
+                continue
+            if my_map[child_loc[0]][child_loc[1]]:
+                continue
+            child = {'loc': child_loc,
+                     'g_val': curr['g_val'] + 1,
+                     'parent': curr,
+                     'timestep': curr['timestep'] + 1}
+
+            if is_constrained(curr['loc'], child['loc'], child['timestep'], constraint_table):
+                continue
+
+            if child['g_val'] > depth:
+                continue
+
+            if (child['loc'], child['timestep']) in closed_list:
+                existing_node = closed_list[(child['loc'], child['timestep'])]
+                if compare(child, existing_node):
+                    closed_list[(child['loc'], child['timestep'])] = child
+                    push(open_list, child)
+            else:
+                closed_list[(child['loc'], child['timestep'])] = child
+                push(open_list, child)
+
+    return None  # Failed to find solutions
+
+def detect_cardinal_conflict(mdd1, mdd2):
+    # Check MDDs for a cardinal conflict
+
+    # If no cardinal conflict is found, choose a semi-cardinal conflict
     # if it was encountered during the iteration
 
-    # Otherwise, a non-cardinal conflict is arbitrarily chosen
-    return collisions[0] # Get first conflict
+    return None
+
 
 def standard_splitting(collision):
     ##############################
@@ -162,12 +226,12 @@ class ICBSSolver(object):
 
     def push_node(self, node):
         heapq.heappush(self.open_list, (node['cost'], len(node['collisions']), self.num_of_generated, node))
-        print("Generate node {}".format(self.num_of_generated))
+        # print("Generate node {}".format(self.num_of_generated))
         self.num_of_generated += 1
 
     def pop_node(self):
         _, _, id, node = heapq.heappop(self.open_list)
-        print("Expand node {}".format(id))
+        # print("Expand node {}".format(id))
         self.num_of_expanded += 1
         return node
 
@@ -206,12 +270,12 @@ class ICBSSolver(object):
         root['collisions'] = detect_collisions(root['paths'])
         self.push_node(root)
 
-        # Task 3.1: Testing
-        print(root['collisions'])
+        # # Task 3.1: Testing
+        # print(root['collisions'])
 
-        # Task 3.2: Testing
-        for collision in root['collisions']:
-            print(standard_splitting(collision))
+        # # Task 3.2: Testing
+        # for collision in root['collisions']:
+        #     print(standard_splitting(collision))
 
         ##############################
         # Task 3.3: High-Level Search
@@ -228,8 +292,23 @@ class ICBSSolver(object):
             # If N has no conflicts then return solution (N is goal)
             if len(next_node['collisions']) == 0:
                 return next_node['paths']
-            # Otherwise, check for cardinal conflict
-            collision = detect_cardinal_conflict(next_node['collisions'])
+            # Iterate over all collisions
+            for conflict in next_node['collisions']:
+                # Build MDD for each agent
+                mdd1 = build_mdd(self.my_map, self.starts[conflict['a1']], self.goals[conflict['a1']], conflict['a1'], next_node['constraints'], next_node['cost'])
+                mdd2 = build_mdd(self.my_map, self.starts[conflict['a2']], self.goals[conflict['a2']], conflict['a2'], next_node['constraints'], next_node['cost'])
+                print("Constraints:", next_node['constraints'])
+                print('MDD1', mdd1)
+                print('MDD2', mdd2)
+                print()
+                # Check for cardinal/semi-cardinal conflict
+                cardinal_conflict = detect_cardinal_conflict(mdd1, mdd2)
+            
+            if cardinal_conflict is not None:
+                collision = cardinal_conflict
+            else:
+                # If no cardinal/semi-cardinal conflict exists, arbitrarily choose a non-cardinal conflict
+                collision = next_node['collisions'][0] # Get first conflict
             constraints = standard_splitting(collision)  # disjoint_splitting(collision)
 
             for constraint in constraints:
